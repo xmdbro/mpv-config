62382ace5df8cb923a8c874fd62a1c25b8e42666
xMdbb <klchiu721@gmail.com>
2022-04-05 18:48:17 +0800

    organized directories & added git ignore

---
+++ portable_config/scripts/osc.lua
@@ -0,0 +1,15 @@
+local osc_styles = {
+    TransBg = '{\\blur100\\bord150\\1c&H000000&\\3c&H000000&}',
+    SeekbarBg = '{\\blur0\\bord0\\1c&HFFFFFF&}',
+    SeekbarFg = '{\\blur1\\bord1\\1c&HE39C42&}',
+    Ctrl1 = '{\\blur0\\bord0\\1c&HFFFFFF&\\3c&HFFFFFF&\\fs36\\fnmaterial-design-iconic-font}',
+    Ctrl2 = '{\\blur0\\bord0\\1c&HFFFFFF&\\3c&HFFFFFF&\\fs24\\fnmaterial-design-iconic-font}',
+    Ctrl2Flip = '{\\blur0\\bord0\\1c&HFFFFFF&\\3c&HFFFFFF&\\fs24\\fnmaterial-design-iconic-font\\fry180',
+    Ctrl3 = '{\\blur0\\bord0\\1c&HFFFFFF&\\3c&HFFFFFF&\\fs24\\fnmaterial-design-iconic-font}',
+    Time = '{\\blur0\\bord0\\1c&HFFFFFF&\\3c&H000000&\\fs17\\fn' .. user_opts.font .. '}',
+    Tooltip = '{\\blur1\\bord0.5\\1c&HFFFFFF&\\3c&H000000&\\fs18\\fn' .. user_opts.font .. '}',
+    Title = '{\\blur1\\bord0.5\\1c&HFFFFFF&\\3c&H0\\fs38\\q2\\fn' .. user_opts.font .. '}',
+    WinCtrl = '{\\blur1\\bord0.5\\1c&HFFFFFF&\\3c&H0\\fs20\\fnmpv-osd-symbols}',
+    elementDown = '{\\1c&H999999&}',
+    elementHighlight = '{\\blur1\\bord1\\1c&HFFC033&}',
+}
